fa99b45d6e907e6874ed969fc7f965e22eb10057
thakur-lalit <lalitrajputkandi@gmail.com>
2025-01-09 12:27:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navbottomapp/ui/theme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.navbottomapp.ui.theme;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
